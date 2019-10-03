ef40c608cddd9759cd24797fc5d6943843a57b9e
JohnReid <johnbaronreid@netscape.net>
2019-10-03 12:09:23 +0100

    Add package setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tfner',
+      version='0.1',
+      description='TensorFlow Named Entity Recognition',
+      url='http://github.com/storborg/funniest',
+      author='Guillaume Genthial and John Reid',
+      author_email='johnbaronreid@netscape.net',
+      license='MIT',
+      packages=['tfner'],
+      zip_safe=False)
